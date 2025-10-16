818c06d5f79f527f6a86d25748d0787e31db9dc6
jonzilinsky <175575173+jonzilinsky@users.noreply.github.com>
2025-10-15 15:20:29 -1000

    added 4_rockpaperscissors

---
+++ MySolutions/4_rockpaperscissors.py
@@ -0,0 +1,40 @@
+import random
+
+global humanscore, cpuscore
+humanscore = 0
+cpuscore = 0
+options = ('r', 'p', 's')
+ddict = {'r':'rock','p':'paper','s':'scissors'}
+gogo = 'yes'
+
+def score():
+  return f'[Human Score: {humanscore} Computer Score: {cpuscore}]'
+
+def play(z):
+  global humanscore, cpuscore, score
+  cpu = random.choice(options)
+  print(f'Computer chooses: {ddict[cpu]} ', end='')
+  if z == cpu:
+    print('Tie!')
+  elif ((z == 'r' and cpu == 's') or
+    (z == 'p' and cpu == 'r') or
+    (z == 's' and cpu == 'p')):
+    humanscore += 1
+    print(f'   Human wins! ', score())
+  else:
+    cpuscore += 1
+    print(f'   Computer wins! ', score())
+
+def main():
+  global gogo
+  human = input('Choose your move: r = rock, p = paper, s = scissors:\n')
+  if human in options:
+    play(human)
+  else:
+    print('\nMove not valid, try again? (yes/no)')
+    gogo = input('')
+
+while gogo == 'yes':
+  main()
+
+print(f'\nFinal:', score())
